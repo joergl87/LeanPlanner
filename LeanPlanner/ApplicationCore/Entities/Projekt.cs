43bbae738c14c1341bbf4056e052f02dda3786c9
joerg.lueddemann <joerg.lueddemann@ditting-bau.de>
2019-04-09 20:42:59 +0200

    Initial Commi

---
+++ LeanPlanner/ApplicationCore/Entities/Projekt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Projekt : BaseEntity
+    {
+        public string Bezeichnung { get; set; }
+        public string Kostenstelle { get; set; }
+        public string Adresse { get; set; }
+        public string Status { get; set; }
+    }
+}
